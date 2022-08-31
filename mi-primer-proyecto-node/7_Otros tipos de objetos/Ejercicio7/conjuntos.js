74e0940d2ccb81a8409e3f1113b7e6ccbab7fb4c
EdwinEcheverry <echeverryedwin@gmail.com>
2022-08-30 21:18:50 -0500

    Ejercicio 7 modificado

--- mi-primer-proyecto-node/7_Otros tipos de objetos/Ejercicio7/conjuntos.js
+++ mi-primer-proyecto-node/7_Otros tipos de objetos/Ejercicio7/conjuntos.js
@@ -1,12 +1,9 @@
 //- Un nuevo Set con los nombres de tu familia
-
-let NamesFamily = ["Arturo", "Yolanda", "Gabriel", "Edwin"]
-
-let setFamily = new Set(NamesFamily)
+let setFamily = new Set(["Arturo", "Yolanda", "Gabriel", "Edwin"])
 
 //- Modifica el Set original añadiendo tu nombre (duplicado) (debería darte lo mismo)
 
-console.log(NamesFamily)
+console.log(setFamily)
 console.log(setFamily.add("Edwin"))
 
 //- Modifica el Set original añadiendo el nombre "Javascript" (ya que empieza a formar parte de tu vida ;)
